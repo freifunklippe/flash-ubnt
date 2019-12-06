e3d3e7829ec73097aa859234d563834fcacd7850
Michael Brinkmann <michael@collimas.net>
2019-12-06 11:30:18 +0100

    Added mehr functionality

--- flash-ubnt.sh
+++ flash-ubnt.sh
@@ -8,6 +8,7 @@
 # functions
 
 flashaclite() {
+ifconfig eth0:0 192.168.1.254 netmask 255.255.255.0 up
 echo
 echo "Der SSH-Key wird nun aktualisiert"
 echo "-------------"
@@ -35,9 +36,27 @@ echo
 echo "Der AP bootet nun und wird zur weiteren Konfiguration in Kürze "
 echo "unter 192.168.1.1 erreichbar sein."
 echo
+printf "%s" "Warten auf Reboot im Config Mode ..."
+while ! ping -c 1 -n -w 1 192.168.1.1 &> /dev/null
+do
+    printf "%c" "."
+done
+printf "\n%s\n"  "Knoten ist online im Config Mode"
+echo "--------------------------------------------" >> nodes.txt
+ip address show eth0 | grep -Eo [:0-9a-f:]{2}\(\:[:0-9a-f:]{2}\){5} >> nodes.txt
+ip address show br-client | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d' >> nodes.txt
+echo "Schreibe SSH Key und Antennen-Offsets"
+cat public_rsa_key.pub | sshpass ssh root@192.168.1.1 'cat >> /etc/dropbear/authorized_keys'
+sshpass ssh root@192.168.1.1 'uci set wireless.radio0.txpower=17'
+sshpass ssh root@192.168.1.1 'uci set wireless.radio1.txpower=14'
+sshpass ssh root@192.168.1.1 'uci commit wireless'
+sshpass ssh root@192.168.1.1 'wifi'
 }
 
+
+
 flashacmesh() {
+ifconfig eth0:0 192.168.1.254 netmask 255.255.255.0 up
 echo
 echo "Der SSH-Key wird nun aktualisiert"
 echo "-------------"
@@ -65,6 +84,67 @@ echo
 echo "Der AP bootet nun und wird zur weiteren Konfiguration in Kürze "
 echo "unter 192.168.1.1 erreichbar sein."
 echo
+printf "%s" "Warten auf Reboot im Config Mode ..."
+while ! ping -c 1 -n -w 1 192.168.1.1 &> /dev/null
+do
+    printf "%c" "."
+done
+printf "\n%s\n"  "Knoten ist online im Config Mode"
+echo "--------------------------------------------" >> nodes.txt
+ip address show eth0 | grep -Eo [:0-9a-f:]{2}\(\:[:0-9a-f:]{2}\){5} >> nodes.txt
+ip address show br-client | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d' >> nodes.txt
+echo "Schreibe SSH Key und Antennen-Offsets"
+cat public_rsa_key.pub | sshpass ssh root@192.168.1.1 'cat >> /etc/dropbear/authorized_keys'
+sshpass ssh root@192.168.1.1 'uci set wireless.radio0.txpower=17'
+sshpass ssh root@192.168.1.1 'uci set wireless.radio1.txpower=14'
+sshpass ssh root@192.168.1.1 'uci commit wireless'
+sshpass ssh root@192.168.1.1 'wifi'
+}
+
+flashacmeshpro() {
+ifconfig eth0:0 192.168.1.254 netmask 255.255.255.0 up
+echo
+echo "Der SSH-Key wird nun aktualisiert"
+echo "-------------"
+ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "192.168.1.20"
+ssh-keyscan "192.168.1.20" >> "/home/$USER/.ssh/known_hosts"
+echo "-------------"
+echo "SSH-Key aktualisiert"
+echo
+echo
+echo "Aktuelle Version des AP:"
+sshpass -p 'ubnt' ssh ubnt@192.168.1.20 'cat /etc/version'
+echo
+echo "Obige Version muss 3.7.58 oder 3.8.3 sein. Wenn eine andere"
+echo "Versionsnummer angezeigt wird, NICHT flashen !"
+echo
+echo "Beliebige Taste drücken - flashen" 
+echo "STRG+C - Script abbrechen"
+read -n 1 TASTE
+sshpass -p 'ubnt' scp acmeshpro.bin ubnt@192.168.1.20:/tmp 
+sshpass -p 'ubnt' ssh ubnt@192.168.1.20 'mtd write /tmp/acmesh.bin kernel0'
+sshpass -p 'ubnt' ssh ubnt@192.168.1.20 'mtd write /tmp/acmesh.bin kernel1'
+sshpass -p 'ubnt' ssh ubnt@192.168.1.20 'dd if=/dev/zero bs=1 count=1 of=/dev/mtd4'
+sshpass -p 'ubnt' ssh ubnt@192.168.1.20 'reboot'
+echo
+echo "Der AP bootet nun und wird zur weiteren Konfiguration in Kürze "
+echo "unter 192.168.1.1 erreichbar sein."
+echo
+printf "%s" "Warten auf Reboot im Config Mode ..."
+while ! ping -c 1 -n -w 1 192.168.1.1 &> /dev/null
+do
+    printf "%c" "."
+done
+printf "\n%s\n"  "Knoten ist online im Config Mode"
+echo "--------------------------------------------" >> nodes.txt
+ip address show eth0 | grep -Eo [:0-9a-f:]{2}\(\:[:0-9a-f:]{2}\){5} >> nodes.txt
+ip address show br-client | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d' >> nodes.txt
+echo "Schreibe SSH Key und Antennen-Offsets"
+cat public_rsa_key.pub | sshpass ssh root@192.168.1.1 'cat >> /etc/dropbear/authorized_keys'
+sshpass ssh root@192.168.1.1 'uci set wireless.radio0.txpower=12'
+sshpass ssh root@192.168.1.1 'uci set wireless.radio1.txpower=12'
+sshpass ssh root@192.168.1.1 'uci commit wireless'
+sshpass ssh root@192.168.1.1 'wifi'
 }
 
 # main
@@ -75,12 +155,12 @@ echo
 echo "Bitte diesen PC unter der IP-Adresse 192.168.1.20/24 erreichbar"
 echo "machen und Ubiquiti Hardware mit dem Netzwerk verbinden."
 echo
-echo "Die zu flashende Firmware muss unter dem Dateinamen acmesh.bin/aclite.bin"
-echo "im Verzeichnis dieses Scriptes liegen."
+echo "Die zu flashende Firmware muss unter dem Dateinamen acmesh.bin,"
+echo "acmeshpro.bin oder aclite.bin im Verzeichnis dieses Scriptes liegen."
 echo
 echo
 PS3='Bitte wählen: '
-options=("Flashen AC Lite" "Flashen AC Mesh" "Beenden")
+options=("Flashen AC Lite" "Flashen AC Mesh" "Flashen AC Mesh Pro" "Beenden")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -93,6 +173,11 @@ do
             echo
             echo "AC Mesh wurde ausgewählt"
             flashacmesh
+            ;;
+        "Flashen AC Mesh Pro")
+            echo
+            echo "AC Mesh Pro wurde ausgewählt"
+            flashacmeshpro
             ;;
         "Beenden")
             break
